9540ec5037b95f2f77335a9e986b947a59e8803a
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-05-30 07:02:34 -0300

    Array and Iterables Tests

--- Documentation/Example-01/types.test.js
+++ Documentation/Example-01/types.test.js
@@ -1,7 +1,22 @@
+//String
 test('does not exist I in word team', () => {
     expect('team').not.toMatch(/I/)
 });
 
 test('but exists "stop" in Christoph', () => {
     expect('Christoph').toMatch(/stop/)
+});
+
+//Arrays and Iterables
+
+test('The buy list should be the item milk', () => {
+    const list = [
+        'fraldas',
+        'kleenex',
+        'sacos de lixo',
+        'papel toalha',
+        'leite',
+    ];
+
+    expect(list).toContain('leite')
 });
