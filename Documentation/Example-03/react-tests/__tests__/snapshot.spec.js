724c0adffd4e5dad128ffc2b23b0e66df8fbd2e3
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-06-09 07:28:38 -0300

    Snapshot Update

--- Documentation/Example-03/react-tests/__tests__/snapshot.spec.js
+++ Documentation/Example-03/react-tests/__tests__/snapshot.spec.js
@@ -1,11 +1,19 @@
-import renderer from 'react-test-renderer';
-
 describe('Snapshot', () => {
-    test('renderer correctly', () => {
+    /*test('renderer correctly', () => {
         const three = renderer.create(
             <a href="http://www.instagram.com">Instagram</a>
         ).toJSON()
 
         expect(three).toMatchSnapshot()
-    });
+    }); */
+
+    it('will fail every time', () => {
+        const user = {
+          createdAt: new Date(),
+          id: Math.floor(Math.random() * 20),
+          name: 'LeBron James',
+        };
+      
+        expect(user).toMatchSnapshot();
+      });
 });
